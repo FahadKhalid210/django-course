5519d067c86d1f4c220a1fc927d2c086e4b43a65
Fahad Khalid <fahad.khalid@arbisoft.com>
2023-04-02 21:01:21 +0500

    fixed views

--- course_list/views.py
+++ course_list/views.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+
 
 
 class GetCourses(APIView):
@@ -14,10 +16,21 @@ class GetCoursesV2(APIView):
     def get(self, request):
         filters = {}
         if disp_name := request.GET.get('name'):
-            filters['display_name'] = disp_name
+            filters['display_name__icontains'] = disp_name
         
         if lang := request.GET.get('lang'):
-            filters['language'] = lang
-
-        queryset =  CourseOverview.get_all_courses(filter_=filters)
-        return Response(list(queryset.values()))
+            filters['language__icontains'] = lang
+        
+        serialized_courses = [] 
+        courses_queryset =  CourseOverview.get_all_courses(filter_=filters)
+        for course in courses_queryset:
+            # Serialize each course and append to the list
+            serialized_courses.append({
+                'name': course.display_name,
+                'language': course.language,
+                'start_date': course.start,
+                'end_date': course.end,
+                # Add any other fields that you want to include
+            })
+            
+        return Response(serialized_courses)
